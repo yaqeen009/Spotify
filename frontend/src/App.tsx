0413356639bb998f46ea0d0d0fedd184706af3be
adamsalimtidoo <adamsalimtidoo@gmail.com>
2025-10-04 09:19:08 +0000

    Initial Commit: Project Set Up

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
+
+function App() {
+
+  return (
+   <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          "100": "#000",
+          "200": "#313131",
+          "300": "#151515",
+          "400": "#424242",
+        },
+        secondary: {
+          "100": "#FFF",
+          "200": "#A3A3A3",
+        },
+        accent: "#87FF72",
+      },
+      fontFamily: {
+        open_sans: ["Open San", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"]
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 8s linear",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
